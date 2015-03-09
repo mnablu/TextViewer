eb2fea35427b383053981825652a9fef7c8d43b1
mnablu <marc.nablu@my.jcu.edu.au>
2015-03-09 11:59:18 +1000

    Initial Commit

---
+++ src/App.java
@@ -0,0 +1,13 @@
+/**
+ * Created by jc219581 on 9/03/15.
+ */
+public class App {
+    public static void main(String[] args) {
+        String text = "This is a simple test\nOne Two Three\nHello World";
+        for (int i = 0; i < text.length(); ++i) {
+            System.out.println(text.charAt(i));
+        }
+        System.out.println();
+
+    }
+}
